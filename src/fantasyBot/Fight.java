483e96c73f8128550b8c31c730034e2867a380fe
KaluNight <lucadasilva003@gmail.com>
2018-08-31 17:21:11 +0200

    Delete ascii file

--- src/fantasyBot/Fight.java
+++ src/fantasyBot/Fight.java
@@ -15,7 +15,7 @@ public class Fight {
 		this.ia = ia;
 		this.channel = channel;
 
-		channel.sendMessage("Vous avez commencé un duel !\n"
+		channel.sendMessage("Vous avez commencÃ© un duel !\n"
 				+ "----------------------------\n"+
 				player.getName() + " contre " + ia.getName() + " !").complete();
 
@@ -35,17 +35,17 @@ public class Fight {
 		if(ia.getActualHealthPoints() <= 0) {
 			endFight();
 		}else {
-			channel.sendMessage("Vous avez infligé " + player.getAttackPoints() + " à " + ia.getName() + " ! "
+			channel.sendMessage("Vous avez infligÃ© " + player.getAttackPoints() + " Ã  " + ia.getName() + " ! "
 					+ "Il lui reste " + ia.getActualHealthPoints() + " sur " + ia.getMaxHealthPoints() + " !").complete();
 			
 			player.setActualHealthPoints(player.getActualHealthPoints() - ia.getAttackPoints());
 
 			if(player.getActualHealthPoints() <= 0) {
-				channel.sendMessage(ia.getName() + " vous a infligé " + ia.getAttackPoints() + " ! Vous mourrez sur le coup !\n"
-						+ "Vous n'êtes pas très fort pour mourir :/");
+				channel.sendMessage(ia.getName() + " vous a infligÃ© " + ia.getAttackPoints() + " ! Vous mourrez sur le coup !\n"
+						+ "Vous n'Ãªtes pas trÃ¨s fort pour mourir :/");
 			}
 			
-			channel.sendMessage(ia.getName() + " vous a infligé " + ia.getAttackPoints() + " ! "
+			channel.sendMessage(ia.getName() + " vous a infligÃ© " + ia.getAttackPoints() + " ! "
 					+ "Il vous reste " + player.getActualHealthPoints() + " sur " + player.getMaxHealthPoints() + " !").complete();
 			
 			choosingPhase();
@@ -54,45 +54,30 @@ public class Fight {
 
 	private void choosingPhase() {
 
-		channel.sendMessage("C'est à " + player.getName() + " de jouer !\n"
-				+ "Votre ennemie possède " + ia.getActualHealthPoints() + " points de vie sur " + ia.getMaxHealthPoints() + ".\n"
+		channel.sendMessage("C'est Ã  " + player.getName() + " de jouer !\n"
+				+ "Votre ennemie possï¿½de " + ia.getActualHealthPoints() + " points de vie sur " + ia.getMaxHealthPoints() + ".\n"
 				+ "Quelle attaque souhaitez-vous effectuer ?\n" 
 				+ "------------------------------------------\n" 
 				+ "1. Attaque basique.").submit();
 	}
 	
 	private void endFight() {
-		channel.sendMessage("Vous avez infligé " + player.getAttackPoints() + " à " + ia.getName() + " ! Il meurt sur ce coup !\n"
-				+ "Félicitation ! Vous remporté : DE L'EXP MAIS PAS TOUS DE SUITE ON TRAVAIL !").complete();
+		channel.sendMessage("Vous avez infligÃ© " + player.getAttackPoints() + " Ã  " + ia.getName() + " ! Il meurt sur ce coup !\n"
+				+ "FÃ©licitation ! Vous remportez : DE L'EXP MAIS PAS TOUS DE SUITE ON TRAVAIL !").complete();
 		
 		EventListener.getFightInProgresse().remove(this);
 		
 	}
 
-
-
 	public Character getPlayer() {
 		return player;
-	}
-
-	public void setPlayer(Character joueur) {
-		player = joueur;
 	}
 
 	public Character getIa() {
 		return ia;
 	}
 
-	public void setIa(Character ia) {
-		this.ia = ia;
-	}
-
 	public PrivateChannel getChannel() {
 		return channel;
 	}
-
-	public void setChannel(PrivateChannel channel) {
-		this.channel = channel;
-	}
-
 }
